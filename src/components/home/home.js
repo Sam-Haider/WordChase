08561d23bc621d245c1f20ace07a4d67f84218d7
Sam-Haider <sammhaider0@gmail.com>
2023-05-10 18:15:55 -0400

    Add instructions Add styling

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,77 +1,137 @@
-import React, { useEffect, useState } from 'react';
-import { words } from '../../wordList';
-import { Keyboard } from '../keyboard/keyboard';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { words } from "../../wordList";
+import { Keyboard } from "../keyboard/keyboard";
+
+const Instructions = styled.div`
+  background: none;
+  border: 1px solid white;
+  color: white;
+  font-size: 18px;
+  padding: 20px;
+  margin-top: 10px;
+  border-radius: 5px;
+  max-width: 310px;
+`;
+
+const Timer = styled.span`
+  color: rgb(221 109 255);
+`;
 
 const updateWord = (letters, gameWords) => {
-    const matches = gameWords.filter(word => {
-        let isMatch = true;
-        letters.forEach((letter, index) => {
-            if(word[index] !== letter){
-                isMatch = false;
-            }
-        })
-        return isMatch;
-    })
+  const matches = gameWords.filter((word) => {
+    let isMatch = true;
+    letters?.forEach((letter, index) => {
+      if (word[index] !== letter) {
+        isMatch = false;
+      }
+    });
+    return isMatch;
+  });
 
-    const newWord = matches[Math.floor(Math.random() * matches?.length)];
-    const newLetter = newWord && newWord[letters.length]
-    return {newWord, newLetter};
-}
+  const newWord = matches[Math.floor(Math.random() * matches?.length)];
+  const newLetter = newWord && newWord[letters.length];
+  return { newWord, newLetter };
+};
 
 export const Home = () => {
-    const [letters, setLetters] = useState([]);
-    const [gameOver, setGameOver] = useState(false);
-    console.log('game over', gameOver);
-    const gameWords = words.filter(word => {
-        return word.length === 5;
-    })
-    const handleClick = e => {
-        const ltr = e.target.value.toLowerCase();
-        const {newWord, newLetter} = updateWord([...letters, ltr], gameWords)
-        console.log('new word is', newWord);
-        console.log('new letter is', newLetter);
-        if(!newWord) {
-            setGameOver(true);
-            return;
-        } else {
-            if(letters.length === 4) {
-                setLetters([...letters, ltr])
-            } else {
-                setLetters([...letters, ltr, newLetter])
-            }
-            
-        }
+  const [letters, setLetters] = useState([]);
+  const [gameOver, setGameOver] = useState(true);
+  const [gameCount, setGameCount] = useState(0);
+  const [score, setScore] = useState(0);
+  const [message, setMessage] = useState("");
+  const gameWords = words.filter((word) => {
+    return word.length === 6;
+  });
+  const handleClick = (e) => {
+    setGameCount(gameCount + 1);
+    const ltr = e.target.value.toLowerCase();
+    const { newWord, newLetter } = updateWord([...letters, ltr], gameWords);
+    if (!newWord) {
+      setScore(score - 1);
+      const { newLetter } = updateWord([], gameWords);
+      setLetters(newLetter);
+      return;
+    } else {
+      if (letters.length === 5) {
+        setLetters([...letters, ltr]);
+      } else {
+        setLetters([...letters, ltr, newLetter]);
+      }
+    }
+  };
+
+  const handlePlay = () => {
+    setGameOver(false);
+    setMessage("");
+    const { newLetter } = updateWord([], gameWords);
+    setLetters([newLetter]);
+    setTime(180);
+    setScore(0);
+  };
+
+  useEffect(() => {
+    if (letters.length === 6) {
+      setScore(score + 1);
+      const { newLetter } = updateWord([], gameWords);
+      setLetters(newLetter);
+    }
+  });
+
+  useEffect(() => {
+    if (!gameOver) {
+      const { newLetter } = updateWord(letters, gameWords);
+      setLetters([newLetter]);
+    }
+  }, [gameOver]);
+
+  const [time, setTime] = useState(60);
+
+  useEffect(() => {
+    if (!gameOver) {
+      time > 0 &&
+        setTimeout(() => {
+          setTime(time - 1);
+        }, 1000);
     }
 
-    const handlePlayAgain = () => {
-        setGameOver(false);
-        setLetters([]);
+    if (time === 0) {
+      setGameOver(true);
+      setMessage("GAME OVER!!!");
     }
+  }, [gameOver, time]);
 
-    useEffect(() => {
-        console.log('letters.length is', letters.length)
-        if(letters.length === 5) {
-            alert('you win');
-        }
-    })
-
-    useEffect(() => {
-        if(gameOver) {
-            alert('you lost');
-        }
-    })
-
-    return (
-        <div className='game-container'>
-            <div className='tile-container'>
-                <div className="tile">{letters[0]}</div>
-                <div className="tile">{letters[1]}</div>
-                <div className="tile">{letters[2]}</div>
-                <div className="tile">{letters[3]}</div>
-                <div className="tile">{letters[4]}</div>
-            </div>
-            <Keyboard gameOver={gameOver} handleClick={handleClick}/>
-            {gameOver && <button className="game-over-btn" onClick={handlePlayAgain}>Play Again</button>}
+  return (
+    <div className="game-container">
+      <div className="time-label">
+        TIME REMAINING: <Timer className="time">{time}</Timer>
+      </div>
+      <div className="score-label">
+        SCORE: <span className="score">{score}</span>
+      </div>
+      <div className="tile-container">
+        <div className="tile">{letters[0]}</div>
+        <div className="tile">{letters[1]}</div>
+        <div className="tile">{letters[2]}</div>
+        <div className="tile">{letters[3]}</div>
+        <div className="tile">{letters[4]}</div>
+        <div className="tile">{letters[5]}</div>
+      </div>
+      <div className="message">{message}</div>
+      <Keyboard gameOver={gameOver} handleClick={handleClick} />
+      <button disabled={!gameOver} className="play-btn" onClick={handlePlay}>
+        PLAY
+      </button>
+      <Instructions>
+        <div>
+          <strong>Instructions: </strong>
         </div>
-      );
-}
+        Think of a 6 letter word that can be made from letters on the screen and
+        play the next letter. The computer goes first and you'll alternate
+        turns. But be careful, the computer might be thinking of a different
+        word than you 😏. 1 point for every word you finish, -1 point if you
+        play a letter that doesn't conform to a 6-letter word.
+      </Instructions>
+    </div>
+  );
+};
--- src/components/keyboard/keyboard.js
+++ src/components/keyboard/keyboard.js
@@ -1,37 +1,218 @@
-import React from 'react';
+import React from "react";
 
-export const Keyboard = ({handleClick, gameOver}) => {
-    console.log('game over is', gameOver)
-    return (
-        <div className="keyboard-container">
-            <div className="keyboard">
-                <button disabled={gameOver}value="A" className="key" onClick={handleClick}>A</button>
-                <button disabled={gameOver}value="B" className="key" onClick={handleClick}>B</button>
-                <button disabled={gameOver}value="C" className="key" onClick={handleClick}>C</button>
-                <button disabled={gameOver}value="D" className="key" onClick={handleClick}>D</button>
-                <button disabled={gameOver}value="E" className="key" onClick={handleClick}>E</button>
-                <button disabled={gameOver}value="F" className="key" onClick={handleClick}>F</button>
-                <button disabled={gameOver}value="G" className="key" onClick={handleClick}>G</button>
-                <button disabled={gameOver}value="H" className="key" onClick={handleClick}>H</button>
-                <button disabled={gameOver}value="I" className="key" onClick={handleClick}>I</button>
-                <button disabled={gameOver}value="J" className="key" onClick={handleClick}>J</button>
-                <button disabled={gameOver}value="K" className="key" onClick={handleClick}>K</button>
-                <button disabled={gameOver}value="L" className="key" onClick={handleClick}>L</button>
-                <button disabled={gameOver}value="M" className="key" onClick={handleClick}>M</button>
-                <button disabled={gameOver}value="N" className="key" onClick={handleClick}>N</button>
-                <button disabled={gameOver}value="O" className="key" onClick={handleClick}>O</button>
-                <button disabled={gameOver}value="P" className="key" onClick={handleClick}>P</button>
-                <button disabled={gameOver}value="Q" className="key" onClick={handleClick}>Q</button>
-                <button disabled={gameOver}value="R" className="key" onClick={handleClick}>R</button>
-                <button disabled={gameOver}value="S" className="key" onClick={handleClick}>S</button>
-                <button disabled={gameOver}value="T" className="key" onClick={handleClick}>T</button>
-                <button disabled={gameOver}value="U" className="key" onClick={handleClick}>U</button>
-                <button disabled={gameOver}value="V" className="key" onClick={handleClick}>V</button>
-                <button disabled={gameOver}value="W" className="key" onClick={handleClick}>W</button>
-                <button disabled={gameOver}value="X" className="key" onClick={handleClick}>X</button>
-                <button disabled={gameOver}value="Y" className="key" onClick={handleClick}>Y</button>
-                <button disabled={gameOver}value="Z" className="key" onClick={handleClick}>Z</button>
-            </div>
-        </div>
-    )
+export const Keyboard = ({ handleClick, gameOver }) => {
+  return (
+    <div className="keyboard-container">
+      <div className="keyboard">
+        <button
+          disabled={gameOver}
+          value="A"
+          className="key"
+          onClick={handleClick}
+        >
+          A
+        </button>
+        <button
+          disabled={gameOver}
+          value="B"
+          className="key"
+          onClick={handleClick}
+        >
+          B
+        </button>
+        <button
+          disabled={gameOver}
+          value="C"
+          className="key"
+          onClick={handleClick}
+        >
+          C
+        </button>
+        <button
+          disabled={gameOver}
+          value="D"
+          className="key"
+          onClick={handleClick}
+        >
+          D
+        </button>
+        <button
+          disabled={gameOver}
+          value="E"
+          className="key"
+          onClick={handleClick}
+        >
+          E
+        </button>
+        <button
+          disabled={gameOver}
+          value="F"
+          className="key"
+          onClick={handleClick}
+        >
+          F
+        </button>
+        <button
+          disabled={gameOver}
+          value="G"
+          className="key"
+          onClick={handleClick}
+        >
+          G
+        </button>
+        <button
+          disabled={gameOver}
+          value="H"
+          className="key"
+          onClick={handleClick}
+        >
+          H
+        </button>
+        <button
+          disabled={gameOver}
+          value="I"
+          className="key"
+          onClick={handleClick}
+        >
+          I
+        </button>
+        <button
+          disabled={gameOver}
+          value="J"
+          className="key"
+          onClick={handleClick}
+        >
+          J
+        </button>
+        <button
+          disabled={gameOver}
+          value="K"
+          className="key"
+          onClick={handleClick}
+        >
+          K
+        </button>
+        <button
+          disabled={gameOver}
+          value="L"
+          className="key"
+          onClick={handleClick}
+        >
+          L
+        </button>
+        <button
+          disabled={gameOver}
+          value="M"
+          className="key"
+          onClick={handleClick}
+        >
+          M
+        </button>
+        <button
+          disabled={gameOver}
+          value="N"
+          className="key"
+          onClick={handleClick}
+        >
+          N
+        </button>
+        <button
+          disabled={gameOver}
+          value="O"
+          className="key"
+          onClick={handleClick}
+        >
+          O
+        </button>
+        <button
+          disabled={gameOver}
+          value="P"
+          className="key"
+          onClick={handleClick}
+        >
+          P
+        </button>
+        <button
+          disabled={gameOver}
+          value="Q"
+          className="key"
+          onClick={handleClick}
+        >
+          Q
+        </button>
+        <button
+          disabled={gameOver}
+          value="R"
+          className="key"
+          onClick={handleClick}
+        >
+          R
+        </button>
+        <button
+          disabled={gameOver}
+          value="S"
+          className="key"
+          onClick={handleClick}
+        >
+          S
+        </button>
+        <button
+          disabled={gameOver}
+          value="T"
+          className="key"
+          onClick={handleClick}
+        >
+          T
+        </button>
+        <button
+          disabled={gameOver}
+          value="U"
+          className="key"
+          onClick={handleClick}
+        >
+          U
+        </button>
+        <button
+          disabled={gameOver}
+          value="V"
+          className="key"
+          onClick={handleClick}
+        >
+          V
+        </button>
+        <button
+          disabled={gameOver}
+          value="W"
+          className="key"
+          onClick={handleClick}
+        >
+          W
+        </button>
+        <button
+          disabled={gameOver}
+          value="X"
+          className="key"
+          onClick={handleClick}
+        >
+          X
+        </button>
+        <button
+          disabled={gameOver}
+          value="Y"
+          className="key"
+          onClick={handleClick}
+        >
+          Y
+        </button>
+        <button
+          disabled={gameOver}
+          value="Z"
+          className="key"
+          onClick={handleClick}
+        >
+          Z
+        </button>
+      </div>
+    </div>
+  );
 };
